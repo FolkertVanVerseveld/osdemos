6d2266573463a134b3252e498e8af1ada54d55d9
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2016-09-05 00:34:25 +0200

    rpi/string: strchr, memcpy and memmove implemented

--- rpi/kernel/string.c
+++ rpi/kernel/string.c
@@ -1,9 +1,38 @@
 #include "string.h"
 
+char *strchr(const char *str, int ch)
+{
+	for (; *str; ++str)
+		if (*str == ch)
+			return str;
+	return NULL;
+}
+
 size_t strlen(const char *str)
 {
 	size_t ret;
 	for (ret = 0; str[ret]; ++ret)
 		;
 	return ret;
+}
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n)
+{
+	unsigned char *d = dest;
+	const unsigned char *s = src;
+	for (; n; --n)
+		*d++ = *s++;
+	return dest;
+}
+
+void *memmove(void *restrict dest, const void *restrict src, size_t n)
+{
+	unsigned char *d = dest;
+	const unsigned char *s = src;
+	if (d <= s)
+		memcpy(dest, src, n);
+	else
+		for (s += n - 1, d += n - 1; n; --n)
+			*d-- = *s--;
+	return dest;
 }
--- rpi/kernel/string.h
+++ rpi/kernel/string.h
@@ -3,6 +3,9 @@
 
 #include <stddef.h>
 
+char *strchr(const char *str, int ch);
 size_t strlen(const char *str);
+void *memcpy(void *restrict dest, const void *restrict src, size_t count);
+void *memmove(void *restrict dest, const void *restrict src, size_t count);
 
 #endif
