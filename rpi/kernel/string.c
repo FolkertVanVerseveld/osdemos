4b9cd4150989e471f7f2678faf4a97f0f43c079c
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2016-09-09 00:01:22 +0200

    rpi/string: strrchr, memchr and memrchr implemented

--- rpi/kernel/string.c
+++ rpi/kernel/string.c
@@ -4,7 +4,33 @@ char *strchr(const char *str, int ch)
 {
 	for (; *str; ++str)
 		if (*str == ch)
-			return str;
+			return (char*)str;
+	return NULL;
+}
+
+char *strrchr(const char *str, int ch)
+{
+	const char *ptr;
+	size_t n = strlen(str);
+	for (ptr = str + n; n; --n)
+		if (*--ptr == ch)
+			return (char*)ptr;
+	return NULL;
+}
+
+void *memchr(const void *ptr, int ch, size_t n)
+{
+	for (const unsigned char *p = ptr; n; --n, ++p)
+		if (*p == ch)
+			return (void*)p;
+	return NULL;
+}
+
+void *memrchr(const void *ptr, int ch, size_t n)
+{
+	for (const unsigned char *p = (const unsigned char*)ptr + n; n; --n, --p)
+		if (*p == ch)
+			return (void*)p;
 	return NULL;
 }
 
--- rpi/kernel/string.h
+++ rpi/kernel/string.h
@@ -4,7 +4,10 @@
 #include <stddef.h>
 
 char *strchr(const char *str, int ch);
+char *strrchr(const char *str, int ch);
 size_t strlen(const char *str);
+void *memchr(const void *ptr, int ch, size_t count);
+void *memrchr(const void *ptr, int ch, size_t count);
 void *memcpy(void *restrict dest, const void *restrict src, size_t count);
 void *memmove(void *restrict dest, const void *restrict src, size_t count);
 
