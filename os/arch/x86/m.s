2d3554419b1412793378d4174526089265b885f2
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2017-04-23 14:43:31 +0200

    poke to address
    Press `a' to start poking to address. You need to specify both the
    segment value and the relative address in that particular segment. E.g.:
    
    a 0800: 0000, ca, fe, ba, be,
    
    Press space to stop poking. Currently, only one byte at a time can be
    poked into memory and backspace is not supported.

--- os/arch/x86/m.s
+++ os/arch/x86/m.s
@@ -16,20 +16,88 @@ main:
 	int 10h
 	mov al, 0xa
 	int 10h
+.loop:
 	; get keystroke
 	mov ah, 0
 	int 16h
 	cmp al, 'a'
 	jz assemble
-	jmp main
+	jmp .loop
 assemble:
 	call put_key
-	jmp main
+	call get_word
+	push ax
+	mov al, ':'
+	call put_key
+	call get_word
+	pop es
+	mov di, ax
+.loop:
+	mov al, ','
+	call put_key
+	push es
+	push di
+	call get_byte
+	pop di
+	pop es
+	cld
+	stosb
+	jmp .loop
 put_key:
 	mov ah, 0xe
 	int 10h
 	mov al, ' '
 	int 10h
 	ret
+get_word:
+	call get_byte
+	mov ah, al
+	push ax
+	call get_byte
+	pop bx
+	mov ah, bh
+	ret
+get_byte:
+	call get_nibble
+	shl ax, byte 4
+	push ax
+	call get_nibble
+	pop bx
+	add al, bl
+	ret
+; ask user input for one xdigit
+; and also print it on screen
+get_nibble:
+	call get_xdigit
+	push ax
+	mov al, bl
+	mov ah, 0xe
+	int 10h
+	pop ax
+	ret
+get_xdigit:
+	mov ah, 0
+	int 16h
+	mov bl, al
+	cmp al, ' '
+	jne .1
+; XXX for some reason, jumping to reset does not work while 0:start does
+	;jmp 0:reset
+	jmp 0:start
+.1:
+	cmp al, '0'
+	jb get_xdigit
+	cmp al, '9'
+	jg .2
+	sub al, '0'
+	ret
+.2:
+	cmp al, 'a'
+	jb get_xdigit
+	cmp al, 'f'
+	ja get_xdigit
+	sub al, 'a' - 10
+	ret
+
 	times 0x200 - 2 - ($ - $$) db 0
 	dw 0xaa55
